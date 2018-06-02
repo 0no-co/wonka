2f2e20b38bc76b0fdfeb03ba5f8408dd466f2896
Marcin Dziewulski <hello@mobily.pl>
2018-06-02 15:55:02 +0200

    Switch from `es6` to `js` (`.index.js`)

--- .index.js
+++ .index.js
@@ -1,4 +1,4 @@
 /* this file is only used as an entrypoint for microbundle;
  * it is in turn used to determine the total bundlesize */
-export * from './lib/es6/src/wonka.js';
-export * from './lib/es6/src/wonkaJs.js';
+export * from './lib/js/src/wonka.js';
+export * from './lib/js/src/wonkaJs.js';
