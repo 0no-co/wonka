857255be82ac12f23a1c03d3ed67377dc665ae4c
Phil Pluckthun <phil@kitten.sh>
2019-01-11 01:32:04 +0000

    Cleanup repo

--- .index.js
+++
@@ -1,4 +0,0 @@
-/* this file is only used as an entrypoint for microbundle;
- * it is in turn used to determine the total bundlesize */
-export * from './lib/es6/src/wonka.js';
-export * from './lib/es6/src/web/wonkaJs.js';
--- site/src/utils/slugify.js
+++
@@ -1,6 +0,0 @@
-const slugs = require(`github-slugger`)()
-
-export const slugify = x => {
-  slugs.reset();
-  return slugs.slug(x);
-};
