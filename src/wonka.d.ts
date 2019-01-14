0359a032b40c4503eea8b33c7ca5d9e479bf62d3
Phil Pluckthun <phil@kitten.sh>
2019-01-14 22:32:28 +0000

    Add typings for mergeMap/switchMap/concatMap/mergeAll/concatAll

--- src/wonka.d.ts
+++ src/wonka.d.ts
@@ -13,10 +13,18 @@ export const map: <A, B>(f: (value: A) => B) => Operator<A, B>;
 export const filter: <A>(f: (value: A) => boolean) => Operator<A, A>;
 export const scan: <A, B>(f: (acc: B, value: A) => B, acc: B) => Operator<A, B>;
 
+export const mergeMap: <A, B>(f: (value: A) => Source<B>) => Operator<A, B>;
+export const switchMap: <A, B>(f: (value: A) => Source<B>) => Operator<A, B>;
+export const concatMap: <A, B>(f: (value: A) => Source<B>) => Operator<A, B>;
+
 export const merge: <A>(sources: Array<Source<A>>) => Source<A>;
 export const concat: <A>(sources: Array<Source<A>>) => Source<A>;
 export const share: <A>(source: Source<A>) => Source<A>;
 export const combine: <A, B>(a: Source<A>, b: Source<B>) => Source<[A, B]>;
+
+export const concatAll: <A>(source: Source<Source<A>>) => Source<A>;
+export const mergeAll: <A>(source: Source<Source<A>>) => Source<A>;
+export const flatten: <A>(source: Source<Source<A>>) => Source<A>;
 
 export const take: <A>(max: number) => Operator<A, A>;
 export const takeLast: <A>(max: number) => Operator<A, A>;
@@ -25,8 +33,6 @@ export const takeUntil: <A>(signal: Source<any>) => Operator<A, A>;
 export const skip: <A>(max: number) => Operator<A, A>;
 export const skipWhile: <A>(f: (value: A) => boolean) => Operator<A, A>;
 export const skipUntil: <A>(signal: Source<any>) => Operator<A, A>;
-
-export const flatten: <A>(source: Source<Source<A>>) => Source<A>;
 
 export const forEach: <A>(f: (x: A) => void) => (source: Source<A>) => void;
 export const subscribe: <A>(f: (x: A) => void) => (source: Source<A>) => (() => void);
