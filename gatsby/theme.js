5817cf55629d6639f9f80482f0bd368cfc36ce2a
Phil Pluckthun <phil@kitten.sh>
2019-07-01 14:01:29 +0100

    Layout fixes and sidebar links (0.2.0)

--- gatsby/theme.js
+++ gatsby/theme.js
@@ -38,7 +38,7 @@ export const fontWeights = ['300', '400', '500', '700', '800'];
 export const letterSpacings = ['normal', '0.01em', '0.05em', '0.1em'];
 export const borderWidths = ['1px', '0.1rem', '0.25rem'];
 export const space = [0, 4, 8, 16, 32, 64];
-export const sizes = [0, 64, 260, 430, 540, 650, 870, 980];
+export const sizes = [0, 64, 260, 430, 540, 650, 870, 980, 1400];
 export const breakpoints = ['55em', '70em'];
 export const radii = [4, 8, 16];
 export const zIndices = [0, 1, 2, 3];
@@ -48,14 +48,3 @@ export const borders = [
   `${borderWidths[1]} solid ${colors.bgPassive}`,
   `${borderWidths[2]} solid ${colors.bgPassive}`
 ];
-
-export const shadows = [
-  'rgba(46, 41, 51, 0.08) 0px 1px 2px, rgba(71, 63, 79, 0.08) 0px 2px 4px',
-  'rgba(0, 0, 0, 0.2) 0 1px 0 0, rgba(0, 0, 0, 0.1) 0 2px 3px 0'
-];
-
-export const mediaQueries = {
-  small: `@media screen and (min-width: ${breakpoints[0]})`,
-  medium: `@media screen and (min-width: ${breakpoints[1]})`,
-  large: `@media screen and (min-width: ${breakpoints[2]})`
-};
