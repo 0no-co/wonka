5a425625ad8b03e8d4c904c4aca6b4e1920a69f0
Phil Pluckthun <phil@kitten.sh>
2022-08-30 01:15:56 +0100

    Implement zip & combine

--- src/combine.ts
+++ src/combine.ts
@@ -1,44 +1,35 @@
 import { Source, SignalKind, TalkbackKind, TalkbackFn } from './types';
 import { push, start, talkbackPlaceholder } from './helpers';
 
-export function combine<A, B>(a: Source<A>, b: Source<B>): Source<[A, B]>;
-export function combine<A, B, C>(a: Source<A>, b: Source<B>, c: Source<C>): Source<[A, B, C]>;
-export function combine<A, B, C, D>(
-  a: Source<A>,
-  b: Source<B>,
-  c: Source<C>,
-  d: Source<D>
+export function zip<T>(sources: Source<T>[]): Source<T[]>;
+export function zip<A, B>(sources: [Source<A>, Source<B>]): Source<[A, B]>;
+export function zip<A, B, C>(sources: [Source<A>, Source<B>, Source<C>]): Source<[A, B, C]>;
+
+export function zip<A, B, C, D>(
+  sources: [Source<A>, Source<B>, Source<C>, Source<D>]
 ): Source<[A, B, C, D]>;
-export function combine<A, B, C, D, E>(
-  a: Source<A>,
-  b: Source<B>,
-  c: Source<C>,
-  d: Source<D>,
-  e: Source<E>
+
+export function zip<A, B, C, D, E>(
+  sources: [Source<A>, Source<B>, Source<C>, Source<D>, Source<E>]
 ): Source<[A, B, C, D, E]>;
-export function combine<A, B, C, D, E, F>(
-  a: Source<A>,
-  b: Source<B>,
-  c: Source<C>,
-  d: Source<D>,
-  e: Source<E>,
-  f: Source<F>
+
+export function zip<A, B, C, D, E, F>(
+  sources: [Source<A>, Source<B>, Source<C>, Source<D>, Source<E>, Source<F>]
 ): Source<[A, B, C, D, E, F]>;
-export function combine<A, B, C, D, E, F, G>(
-  a: Source<A>,
-  b: Source<B>,
-  c: Source<C>,
-  d: Source<D>,
-  e: Source<E>,
-  f: Source<F>,
-  g: Source<G>
+
+export function zip<A, B, C, D, E, F, G>(
+  sources: [Source<A>, Source<B>, Source<C>, Source<D>, Source<E>, Source<F>, Source<G>]
 ): Source<[A, B, C, D, E, F, G]>;
 
-export function combine(...sources: Source<unknown>[]): Source<unknown[]> {
+export function zip<A, B, C, D, E, F, G, H>(
+  sources: [Source<A>, Source<B>, Source<C>, Source<D>, Source<E>, Source<F>, Source<G>, Source<H>]
+): Source<[A, B, C, D, E, F, G, H]>;
+
+export function zip<T>(sources: Source<T>[]): Source<T[]> {
   return sink => {
     const filled: boolean[] = new Array(sources.length).fill(false);
     const talkbacks: TalkbackFn[] = new Array(sources.length).fill(talkbackPlaceholder);
-    const buffer: unknown[] = new Array(sources.length).fill(undefined);
+    const buffer: T[] = new Array(sources.length).fill(undefined);
 
     let gotBuffer = false;
     let gotSignal = false;
@@ -88,4 +79,56 @@ export function combine(...sources: Source<unknown>[]): Source<unknown[]> {
       })
     );
   };
+}
+
+export function combine<A, B>(a: Source<A>, b: Source<B>): Source<[A, B]>;
+export function combine<A, B, C>(a: Source<A>, b: Source<B>, c: Source<C>): Source<[A, B, C]>;
+
+export function combine<A, B, C, D>(
+  a: Source<A>,
+  b: Source<B>,
+  c: Source<C>,
+  d: Source<D>
+): Source<[A, B, C, D]>;
+
+export function combine<A, B, C, D, E>(
+  a: Source<A>,
+  b: Source<B>,
+  c: Source<C>,
+  d: Source<D>,
+  e: Source<E>
+): Source<[A, B, C, D, E]>;
+
+export function combine<A, B, C, D, E, F>(
+  a: Source<A>,
+  b: Source<B>,
+  c: Source<C>,
+  d: Source<D>,
+  e: Source<E>,
+  f: Source<F>
+): Source<[A, B, C, D, E, F]>;
+
+export function combine<A, B, C, D, E, F, G>(
+  a: Source<A>,
+  b: Source<B>,
+  c: Source<C>,
+  d: Source<D>,
+  e: Source<E>,
+  f: Source<F>,
+  g: Source<G>
+): Source<[A, B, C, D, E, F, G]>;
+
+export function combine<A, B, C, D, E, F, G, H>(
+  a: Source<A>,
+  b: Source<B>,
+  c: Source<C>,
+  d: Source<D>,
+  e: Source<E>,
+  f: Source<F>,
+  g: Source<G>,
+  h: Source<H>
+): Source<[A, B, C, D, E, F, G, H]>;
+
+export function combine<T>(...sources: Source<T>[]): Source<T[]> {
+  return zip(sources);
 }
