9c062ac32ac69092ace296295532297173835a4c
Phil Pluckthun <phil@kitten.sh>
2023-02-10 13:54:09 +0000

    feat: Implement bundled d.ts output (#135)

--- scripts/flow-typings-plugin.mjs
+++ scripts/flow-typings-plugin.mjs
@@ -1,23 +1,28 @@
-import { resolve, basename, dirname, join } from 'path';
-import { writeFileSync } from 'fs';
 import { compiler, beautify } from 'flowgen';
-
-import glob from 'glob';
 
 function flowTypings() {
   return {
     name: 'flow-typings',
-    async writeBundle() {
-      const cwd = process.cwd();
-      for (const file of glob.sync('dist/types/**/*.d.ts')) {
-        const fullpath = resolve(cwd, file);
-        const flowdef = beautify(compiler.compileDefinitionFile(fullpath));
-        const name = basename(fullpath, '.d.ts');
-        const filepath = dirname(fullpath);
-        const newpath = join(filepath, name + '.js.flow');
-        const definition = flowdef.replace(/import/g, 'import type');
-        writeFileSync(newpath, '// @flow\n\n' + definition);
+    async renderChunk(code, chunk) {
+      if (chunk.fileName.endsWith('d.ts')) {
+        // NOTE: Computed property names will be omitted
+        code = code.replace(/\[Symbol\.\w+\][?()]*:(?:.*);\n?/g, '');
+
+        let flowdef = compiler.compileDefinitionString(code);
+
+        flowdef = beautify(flowdef);
+        flowdef = flowdef.replace(/import/g, 'import type');
+        flowdef = `// @flow\n\n${flowdef}`;
+
+        this.emitFile({
+          type: 'asset',
+          name: chunk.name,
+          fileName: `${chunk.name}.js.flow`,
+          source: flowdef,
+        });
       }
+
+      return null;
     },
   };
 }
