a8ec54d293520758fce8f4b5c1c40969b648c6ed
Phil Pluckthun <phil@kitten.sh>
2023-02-10 14:16:33 +0000

    Add full TSDocs for sinks

--- src/sinks.ts
+++ src/sinks.ts
@@ -103,6 +103,27 @@ export function publish<T>(source: Source<T>): void {
 
 const doneResult = { done: true } as IteratorReturnResult<void>;
 
+/** Converts a Source to an AsyncIterable that pulls and issues values from the Source.
+ *
+ * @param source - A {@link Source}.
+ * @returns An {@link AsyncIterable | `AsyncIterable`} issuing values from the Source.
+ *
+ * @remarks
+ * `toAsyncIterable` will create an {@link AsyncIterable} that pulls and issues values from a given
+ * {@link Source}. This can be used in many interoperability situations, to provide an iterable when
+ * a consumer requires it.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols#the_async_iterator_and_async_iterable_protocols}
+ * for the JS Iterable protocol.
+ *
+ * @example
+ * ```ts
+ * const iterable = toAsyncIterable(fromArray([1, 2, 3]));
+ * for await (const value of iterable) {
+ *   console.log(value); // outputs: 1, 2, 3
+ * }
+ * ```
+ */
 export const toAsyncIterable = <T>(source: Source<T>): AsyncIterable<T> => ({
   [Symbol.asyncIterator](): AsyncIterator<T> {
     const buffer: T[] = [];
@@ -152,19 +173,17 @@ export const toAsyncIterable = <T>(source: Source<T>): AsyncIterable<T> => ({
  * @returns An array of values collected from the {@link Source}.
  *
  * @remarks
- * `subscribe` accepts a `subscriber` callback and returns a function accepting a {@link Source}.
- * When a source is passed to the returned funtion, the subscription will start and `subscriber`
- * will be called for each new value the Source issues. This will also return a {@link Subscription}
- * object that can cancel the ongoing {@link Source} early.
+ * `toArray` accepts a {@link Source} and returns an array of all synchronously issued values from
+ * this Source. It will issue {@link TalkbackKind.Pull | Pull signals} after every value it receives
+ * and expects the Source to recursively issue values.
+ *
+ * Any asynchronously issued values will not be
+ * added to the array and a {@link TalkbackKind.Close | Close signal} is issued by the sink before
+ * returning the array.
  *
  * @example
  * ```ts
- * const subscription = pipe(
- *   fromValue('test'),
- *   subscribe(text => {
- *     console.log(text); // 'test'
- *   })
- * );
+ * toArray(fromArray([1, 2, 3])); // [1, 2, 3]
  * ```
  */
 export function toArray<T>(source: Source<T>): T[] {
@@ -185,6 +204,26 @@ export function toArray<T>(source: Source<T>): T[] {
   return values;
 }
 
+/** Subscribes to a given source and returns a Promise that will resolve with the last value the
+ * source issues.
+ *
+ * @param source - A {@link Source}.
+ * @returns A {@link Promise} resolving to the last value of the {@link Source}.
+ *
+ * @remarks
+ * `toPromise` will subscribe to the passed {@link Source} and resolve to the last value of it once
+ * it receives the last value, as signaled by the {@link SignalKind.End | End signal}.
+ *
+ * To keep its implementation simple, padding sources that don't issue any values to `toPromise` is
+ * undefined behaviour and `toPromise` will issue `undefined` in that case.
+ *
+ * The returned {@link Promise} delays its value by a microtick, using `Promise.resolve`.
+ *
+ * @example
+ * ```ts
+ * toPromise(fromValue('test')); // resolves: 'test'
+ * ```
+ */
 export function toPromise<T>(source: Source<T>): Promise<T> {
   return new Promise(resolve => {
     let talkback = talkbackPlaceholder;
